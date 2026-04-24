0fca9cffcde43373213d24f12745f754f4b882ff
ragalla <ragallaarchana7@gmail.com>
2026-04-24 16:50:18 +0530

    fix database config for render

--- resume_project/settings.py
+++ resume_project/settings.py
@@ -76,39 +76,41 @@ WSGI_APPLICATION = 'resume_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-import os
-if os.environ.get("RENDER"): 
-    # Production (Render + Railway MySQL) 
-    DATABASES = { 
-        "default": { 
-            "ENGINE": "django.db.backends.mysql", 
-            "NAME": os.environ.get("MYSQLDATABASE"), 
-            "USER": os.environ.get("MYSQLUSER"), 
-            "PASSWORD": os.environ.get("MYSQLPASSWORD"), 
-            "HOST": os.environ.get("MYSQLHOST"), 
-            "PORT": os.environ.get("MYSQLPORT", "3306"), 
-        } 
-    } 
- 
-else: 
-    # Local development 
-    DATABASES = { 
-        "default": { 
-            "ENGINE": "django.db.backends.mysql", 
-            "NAME": "college", 
-            "USER": "root", 
-            "PASSWORD": "Archana@14", 
-            "HOST": "localhost", 
-            "PORT": "3306", 
-        } 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
+
+
+
+# import os
+# if os.environ.get("RENDER"): 
+#     # Production (Render + Railway MySQL) 
+#     DATABASES = { 
+#         "default": { 
+#             "ENGINE": "django.db.backends.mysql", 
+#             "NAME": os.environ.get("MYSQLDATABASE"), 
+#             "USER": os.environ.get("MYSQLUSER"), 
+#             "PASSWORD": os.environ.get("MYSQLPASSWORD"), 
+#             "HOST": os.environ.get("MYSQLHOST"), 
+#             "PORT": os.environ.get("MYSQLPORT", "3306"), 
+#         } 
+#     } 
+ 
+# else: 
+#     # Local development 
+#     DATABASES = { 
+#         "default": { 
+#             "ENGINE": "django.db.backends.mysql", 
+#             "NAME": "college", 
+#             "USER": "root", 
+#             "PASSWORD": "Archana@14", 
+#             "HOST": "localhost", 
+#             "PORT": "3306", 
+#         } 
+#     }
 
 
 # Password validation
